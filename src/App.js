2e9a558c130b3cfabca9927f66508e79c24adfb9
ranokhassanova <khasanova.rano@gmail.com>
2023-11-28 10:14:42 +0600

    api-call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
-import Navbar from "./components/Navbar";
-import './App.css'
-
-
+import { Navbar, Cryptocurrencies, News, Exchanges, Homepage, CryptoDetails } from "./components";
+import './App.css';
 
 const App = ()=> {
     return (
@@ -13,8 +11,32 @@ const App = ()=> {
             <div className="navbar">
                 <Navbar/>
             </div>
-            <div className="main"></div>
-            <div className="footer"></div>
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                    <Routes>
+                        <Route path='/' element={<Homepage/>}/>
+                        <Route path='/exchanges' element={<Exchanges/>}/>
+                        <Route path='/cryptocurrencies' element={<Cryptocurrencies/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/crypto/:coinId' element={<CryptoDetails/>}/>
+                    </Routes>
+                    </div>
+                </Layout>
+                
+                <div className="footer">
+                    <Typography.Title level={4} style={{color: '#fff', textAlign: 'center'}}>
+                        Crypton <br/>
+                        All rights reserrved
+                    </Typography.Title>
+                    <Space style={{color: '#fff', textAlign: 'center'}}>
+                        <Link to='/'>Home</Link>
+                        <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
+            </div>
         </div>
     )
 }
